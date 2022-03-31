fixing covariance growing

pcdet/models/dense_heads/anchor_head_single.py
@@ -29,9 +29,12 @@ class AnchorHeadSingle(AnchorHeadTemplate):
             #in our case N = 7 (seven parameters to describe the bounding box)
             # we will only implement diagonal here (not full covariance)
             self.conv_var = nn.Conv2d(
-                input_channels, self.num_anchors_per_location * 7,
+                input_channels, self.num_anchors_per_location * self.box_coder.code_size,
                 kernel_size=1
             )
+
+            self.logvar_max = self.model_cfg.get("LOGVAR_MAX", None)
+
             ## for full covariance approach (does not work in pytorch)
             # self.conv_var = nn.Conv2d(
             #     input_channels, self.num_anchors_per_location * 28,
@@ -58,8 +61,7 @@ class AnchorHeadSingle(AnchorHeadTemplate):
         nn.init.constant_(self.conv_cls.bias, -np.log((1 - pi) / pi))
         nn.init.normal_(self.conv_box.weight, mean=0, std=0.001)
         if self.model_cfg.VAR_OUTPUT_REG:
-            nn.init.constant_(self.conv_var.bias, 0)
-            nn.init.normal_(self.conv_var.weight, mean=0, std=0.0001)
+            nn.init.normal_(self.conv_var.weight, mean=0, std=0.001)
         if self.model_cfg.VAR_OUTPUT_CLS:
             nn.init.normal_(self.conv_cls_var.weight, mean=0, std=0.01)
             nn.init.constant_(self.conv_cls_var.bias, -10.0)
@@ -74,20 +76,23 @@ class AnchorHeadSingle(AnchorHeadTemplate):
         cls_preds = cls_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
         box_preds = box_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
  
-
+        # variance probablities
         if self.model_cfg.VAR_OUTPUT_REG:
             box_var_preds = self.conv_var(spatial_features_2d)
             box_var_preds = box_var_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
+            if self.logvar_max is not None:
+                box_var_preds = torch.clamp(box_var_preds, max=self.logvar_max)
+            box_var_preds = torch.exp(box_var_preds)
             self.forward_ret_dict['box_var_preds'] = box_var_preds
+        else:
+            box_var_preds = None
 
+        # classification probabilities
         if self.model_cfg.VAR_OUTPUT_CLS:
             cls_var_preds = self.conv_cls_var(spatial_features_2d)
             cls_var_preds = cls_var_preds.permute(0, 2, 3, 1).contiguous()  # [N, H, W, C]
             self.forward_ret_dict['cls_var_preds'] = cls_var_preds
-
-            
-        self.forward_ret_dict['cls_preds'] = cls_preds
-        self.forward_ret_dict['box_preds'] = box_preds
+    
 
         if self.conv_dir_cls is not None:
             dir_cls_preds = self.conv_dir_cls(spatial_features_2d)
@@ -95,6 +100,9 @@ class AnchorHeadSingle(AnchorHeadTemplate):
             self.forward_ret_dict['dir_cls_preds'] = dir_cls_preds
         else:
             dir_cls_preds = None
+
+        self.forward_ret_dict['cls_preds'] = cls_preds
+        self.forward_ret_dict['box_preds'] = box_preds
 
         if self.training:
             targets_dict = self.assign_targets(